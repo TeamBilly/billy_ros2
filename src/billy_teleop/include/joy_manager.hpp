fcd85633dbc111562faebd454e414b5d1c4aafac
adrien <a.drapbiz@gmail.com>
2022-04-21 00:07:40 +0200

    Add the pkg joy_manager which will handle the joy inputs

---
+++ src/billy_teleop/include/joy_manager.hpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class JoyManager : public rclcpp::Node
+{
+  public:
+    JoyManager();
+
+  private:
+    void timer_callback();
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
