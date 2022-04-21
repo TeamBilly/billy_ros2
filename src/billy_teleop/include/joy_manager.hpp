2252ca84aad8888e95e47a95e77d6d9f205bf3a7
adrien <a.drapbiz@gmail.com>
2022-04-21 23:44:48 +0200

    Add a rate limiter for, guess what, limiting the rate of the speed!

--- src/billy_teleop/include/joy_manager.hpp
+++ src/billy_teleop/include/joy_manager.hpp
@@ -5,11 +5,18 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/joy.hpp"
 
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
+
+#define RISING_SLEW_RATE 1
+#define FALLING_SLEW_RATE -1
+#define SPEED_MAX 10
+#define TURN_MAX 30
+#define VERBOSE 0
 
 class JoyManager : public rclcpp::Node
 {
@@ -17,8 +24,24 @@ class JoyManager : public rclcpp::Node
     JoyManager();
 
   private:
-    void timer_callback();
+    void createPublishers();
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    void timer_callback();
+
+    void createSubscribers();
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_joy_;
+    void callbackJoy(sensor_msgs::msg::Joy::SharedPtr msg);
+
+    // Variables
+    float joy_turn_;
+    float joy_speed_;
+    float joy_speed_previous_;
+
+    float rateLimiter(float input, float output_previous);
+    std::chrono::steady_clock::time_point rate_limiter_previous_time_;
+
     size_t count_;
+
+
 };
--- src/billy_teleop/src/JoyManager.cpp
+++ src/billy_teleop/src/JoyManager.cpp
@@ -1,12 +1,23 @@
 #include "joy_manager.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 JoyManager::JoyManager() : Node("joy_manager_node"), count_(0)
 {
+    createPublishers();
+    createSubscribers();
+}
+
+void JoyManager::createPublishers()
+{
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-    500ms, std::bind(&JoyManager::timer_callback, this));
+    timer_ = this->create_wall_timer(500ms, std::bind(&JoyManager::timer_callback, this));
+}
+
+void JoyManager::createSubscribers()
+{
+    subscriber_joy_ = this->create_subscription<sensor_msgs::msg::Joy>("/joy", 10, std::bind(&JoyManager::callbackJoy, this, _1));
 }
 
 void JoyManager::timer_callback()
@@ -15,4 +26,49 @@ void JoyManager::timer_callback()
     message.data = "Hello, world! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+}
+
+void JoyManager::callbackJoy(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+    joy_speed_ = msg->axes[1];
+    joy_turn_ = msg->axes[3];
+
+    joy_speed_ *= SPEED_MAX;
+    joy_turn_ *= TURN_MAX;
+    joy_speed_previous_ = rateLimiter(joy_speed_, joy_speed_previous_);
+}
+
+float JoyManager::rateLimiter(float input, float output_previous)
+{
+    // Computing time difference
+    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_diff = 
+      std::chrono::duration_cast<std::chrono::duration<double>>(now - rate_limiter_previous_time_);
+
+
+    // Compute the rate
+    float rate = (input - output_previous) / time_diff.count();
+
+    // Evaluating rate
+    float output = output_previous;
+    if(rate > RISING_SLEW_RATE) {
+        output = time_diff.count()*RISING_SLEW_RATE + output_previous;
+    } else if (rate < FALLING_SLEW_RATE) {
+        output = time_diff.count()*FALLING_SLEW_RATE + output_previous;
+    } else {
+        output = input;
+    }
+
+    if (VERBOSE >= 2) {
+        RCLCPP_INFO(this->get_logger(), "Time diff: '%f'", time_diff.count());    
+        RCLCPP_INFO(this->get_logger(), "Rate: '%f'", rate);    
+        RCLCPP_INFO(this->get_logger(), "Input: '%f'", input);    
+        RCLCPP_INFO(this->get_logger(), "Output: '%f'", output);    
+    }
+
+    // Preparing time for next input
+    rate_limiter_previous_time_ = now;
+
+    return output;
+
 }
